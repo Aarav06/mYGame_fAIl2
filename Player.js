e5378868c03d5aba57729ef428c8c858031f68dd
Aarav06 <74205009+Aarav06@users.noreply.github.com>
2021-02-01 14:10:05 +0000

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,50 @@
+class Player {
+    constructor(){
+      this.index = null;
+      this.distance = 0;
+      this.name = null;
+      this.points = 0;
+    }
+
+    //get count from database
+    getCount(){
+      var playerCountRef = database.ref('playerCount');
+      playerCountRef.on("value",(data)=>{
+        playerCount = data.val();
+      })
+    }
+  
+    //update count from database
+    updateCount(count){
+      database.ref('/').update({
+        playerCount: count
+      });
+    }
+
+    
+
+    //update distance and name to database
+    update(){
+      var playerIndex = "players/player" + this.index;
+      database.ref(playerIndex).set({
+        name:this.name,
+        distance:this.distance,
+        points:this.points,
+      });
+    }
+
+    updatePoint(point){
+      var playerIndex = "players/player" + this.index;
+      database.ref(playerIndex).update({
+        points: point
+      });
+    }
+
+    //gets player info from database
+    static getPlayerInfo(){
+      var playerInfoRef = database.ref('players');
+      playerInfoRef.on("value",(data)=>{
+        allPlayers = data.val();
+      })
+    }
+  }
